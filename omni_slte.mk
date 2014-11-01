8f1fa6bed8bd27e59fcbb83d55f61fc153c2f061
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-11-02 00:28:22 +0530

    change to omni

--- omni_slte.mk
+++ omni_slte.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/merk/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := slte
 
 # Inherit some common merk stuff.
-$(call inherit-product, vendor/merk/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/slte/device_slte.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := slte
-PRODUCT_NAME := merk_slte
+PRODUCT_NAME := omni_slte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := slte
 PRODUCT_MANUFACTURER := samsung
